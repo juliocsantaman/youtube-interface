da2fccccac4df59ca041ba2e7aa88c6dabaaa231
juliocsantaman <juliocsantaman@gmail.com>
2021-06-13 03:36:54 -0500

    I did sidebar, it is first version.

---
+++ js/index.js
@@ -0,0 +1,6 @@
+// const faBars = document.getElementById('fa-bars');
+// const menu = document.getElementById('menu');
+
+// faBars.addEventListener('click', function() {
+//     menu.classList.toggle('inactive-menu');
+// });
